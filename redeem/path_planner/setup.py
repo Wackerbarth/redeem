ccad7fcfbc2b8dd68f5233f129ecb3809af80325
Richard Wackerbarth <rkw@dataplex.net>
2019-05-06 06:30:50 -0500

    Why does this setup.py even exist?

--- redeem/path_planner/setup.py
+++ redeem/path_planner/setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-
-from distutils.core import setup, Extension
-
+import numpy as np
 import os
+import pip
+from distutils.core import setup, Extension
 from distutils.sysconfig import get_config_vars
 
 (opt, ) = get_config_vars('OPT')
